766c5bf5ca9d9ae2c4330e3fc9665928a3d7b38a
ZefDev <vadim.mandrik.97@gmail.com>
2021-10-06 22:41:24 +0300

    fix bug with dropbox image disapear a few hours

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -8,6 +8,10 @@ use Exception;
 use Illuminate\Support\Facades\Storage;
 use InvalidArgumentException;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\ServiceProvider;
+use Spatie\Dropbox\Client as Client;
+use Spatie\FlysystemDropbox\DropboxAdapter;
+
 
 class ImageService
 {
@@ -23,9 +27,28 @@ class ImageService
 
     public function saveImages($data,$task_id){
         foreach ($data as $file) {
+//            $temp_data = array();
+//            $temp_link =Storage::disk('dropbox')->put('', $file);
+//            $temp_data['link'] = Storage::disk('dropbox')->url($temp_link);
+//            $temp_data['task_id'] = $task_id;
+//            $this->imageRepository->save($temp_data);
+//            $temp_link =Storage::disk('dropbox')->put('', $file);
+//
+//            $filename = '/text1.txt';
+//            $adapter = \Storage::disk('dropbox')->getAdapter();
+//            $client = $adapter->getClient();
+//            //dd($client);
+//            $link = $client->createTemporaryDirectLink($filename);
+//            //dd($temp_link);
             $temp_data = array();
-            $temp_link =Storage::disk('dropbox')->put('', $file);
-            $temp_data['link'] = Storage::disk('dropbox')->url($temp_link);
+            $client = new Client("G1RyECevtnAAAAAAAAAAAa9SK-Nx766sPNcRmvWkJ_GqI27hurs7DFUHPXoNJ60e");
+            $temp_link = "/".Storage::disk('dropbox')->put('', $file);
+            $arr = array( "audience"=> "public",
+                "access"=> "viewer",
+                "requested_visibility"=> "public",
+                "allow_download"=> true);
+            $sharedLink = str_replace("?dl=0", "?raw=1", $client->createSharedLinkWithSettings($temp_link,$arr)['url']);
+            $temp_data['link'] = $sharedLink;
             $temp_data['task_id'] = $task_id;
             $this->imageRepository->save($temp_data);
         }
